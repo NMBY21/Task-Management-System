4aa17d7542a1b0d26f8304e67ea5a53019af321c
NMBY21 <betelihemmengist21@gmail.com>
2025-02-26 20:04:04 +0300

    task-management

--- task-manager-frontend/src/App.jsx
+++ task-manager-frontend/src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import Login from './pages/Login';
-// import home from './pages/home';
 function App() {
   return (
     <Router>
       <Routes>
-        {/* <Route path='/'element={<home />}/> */}
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Routes>
